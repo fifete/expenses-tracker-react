be1c430b68089471b3df12b227d66d097843d80d
day all <danielafuentesdev@gmail.com>
2023-01-24 11:32:55 -0500

    styles: layout in welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,31 +5,31 @@ export const Welcome = ({
   handleShow
 }) => {
   return (
-    <div className='welcome container-limits' id='welcome'>
-      <div>
+    <div className='welcome flex container-limits' id='welcome'>
+      <div className='flex justify-content-between align-items-start'>
         <div>
           <h5 className='fs-small fw-300'>Hello,</h5>
-          <span className='name ff-remark fs-500'>Daniela</span>
+          <span className='name ff-remark fs-500 fw-600'>Daniela</span>
         </div>
-        <button className='btn btn-traslucid ff-remark fs-small'>Log In</button>
+        <button className='btn btn-traslucid ff-remark fs-small fw-600'>Log In</button>
       </div>
       <div>
         <h5 className='fs-small fw-300'>Expenses Today</h5>
         <h1 className='ff-price fs-big'>$29.4</h1>
       </div>
-      <div className='ff-remark'>
-        <div>
+      <div className='welcome-buttons flex ff-remark'>
+        <div className='welcome-btn-container grid'>
           <span
-            className='btn btn-traslucid'
+            className='btn btn-traslucid flex justify-content-center align-items-center'
             onClick={() => handleShow(true)}
           >
-            <i className="uil uil-plus"></i>
+            <i className="uil uil-plus fs-500 fw-600"></i>
           </span>
           <p className='fs-smallest'>Expense</p>
         </div>
-        <div>
-          <span className='btn btn-traslucid'>
-            <i className="uil uil-graph-bar"></i>
+        <div className='welcome-btn-container grid'>
+          <span className='btn btn-traslucid flex justify-content-center align-items-center'>
+            <i className="uil uil-graph-bar fs-500 fw-600"></i>
           </span>
           <p className='fs-smallest'>Stats</p>
         </div>

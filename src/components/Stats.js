de4c2a620d177f2fb76270323d864e50e9f8a71e
Daniela <danielaFuentesDev@hotmail.com>
2023-02-10 15:31:55 -0500

    style: center and define size of no expenses message in stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -28,45 +28,54 @@ export const Stats = () => {
           className='uil uil-angle-left-b'
           onClick={() => navigate("/home")}></i>
       </div>
-      <div className='flex stats-top'>
-        <h2 className='fs-500'>My expenses</h2>
-        <div className='ff-remark fs-smallest'>
-          <span>Today</span>
-          <i className='uil uil-angle-down'></i>
-        </div>
-      </div>
-      <div className='flex center stats-daily-expense'>
-        <span className="ff-price fs-500">${totalDailySpent}</span>
-        <PieChart width={150} height={200}>
-          <Pie
-            data={categoriesAmount}
-            cx={75}
-            cy={100}
-            innerRadius={60}
-            outerRadius={70}
-            paddingAngle={2}
-            dataKey="spending_amount"
-          >
-            {categoriesAmount.map(category => {
-              return <Cell key={category.id} fill={category.color} />
-            })}
-          </Pie>
-          <Tooltip />
-        </PieChart>
-      </div>
-      <div className='custom-stack-1'>
-        {
-          categoriesAmount.map(category => (
-            <div className='flex card-custom center stats-category'>
-              <div className='flex center'>
-                <div style={{ backgroundColor: category.color }} className='category-circle'></div>
-                <p className='fs-small fw-500'>{category.name}</p>
+      {
+        categoriesAmount.length !== 0 ?
+          <>
+            <div className='flex stats-top'>
+              <h2 className='fs-500'>My expenses</h2>
+              <div className='ff-remark fs-smallest'>
+                <span>Today</span>
+                <i className='uil uil-angle-down'></i>
               </div>
-              <span className='ff-price'>${category.spending_amount}</span>
             </div>
-          ))
-        }
-      </div>
+            <div className='flex center stats-daily-expense'>
+              <span className="ff-price fs-500">${totalDailySpent}</span>
+              <PieChart width={150} height={200}>
+                <Pie
+                  data={categoriesAmount}
+                  cx={75}
+                  cy={100}
+                  innerRadius={60}
+                  outerRadius={70}
+                  paddingAngle={2}
+                  dataKey="spending_amount"
+                >
+                  {categoriesAmount.map(category => {
+                    return <Cell key={category.id} fill={category.color} />
+                  })}
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </div>
+            <div className='custom-stack-1'>
+              {
+                categoriesAmount.map(category => (
+                  <div className='flex card-custom center stats-category'>
+                    <div className='flex center'>
+                      <div style={{ backgroundColor: category.color }} className='category-circle'></div>
+                      <p className='fs-small fw-500'>{category.name}</p>
+                    </div>
+                    <span className='ff-price'>${category.spending_amount}</span>
+                  </div>
+                ))
+              }
+            </div>
+          </> :
+          <div class="custom-stack-1-5 stats-fails">
+            <h3> No expenses yet</h3>
+            <img src='/oh-no.webp' alt='oh no giph' />
+          </div>
+      }
     </div>
   )
 }
